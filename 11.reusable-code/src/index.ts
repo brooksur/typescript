01c7335e58b1eede3863e95aa3a68c82aadcc059
Brooks <brooks@pdrutah.com>
2022-08-13 14:26:59 -0600

    11.reusable-code: CsvFileReader for MatchData

--- 11.reusable-code/src/index.ts
+++ 11.reusable-code/src/index.ts
@@ -1,23 +1,13 @@
-import fs from 'fs'
+import { CsvFileReader } from './CsvFileReader'
+import { MatchResult } from './MatchResult'
 
-const matches = fs
-  .readFileSync('football.csv', {
-    encoding: 'utf-8'
-  })
-  .split('\n')
-  .map((row: string): string[] => {
-    return row.split(',')
-  })
+const footballReader = new CsvFileReader('football.csv')
 
-enum MatchResult {
-  HomeWin = 'H',
-  AwayWin = 'A',
-  Draw = 'D'
-}
+footballReader.read()
 
 let manUnitedWins = 0
 
-for (let match of matches) {
+for (let match of footballReader.data) {
   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWins++
   } else if (match[1] === 'Man United' && match[5] === MatchResult.AwayWin) {
