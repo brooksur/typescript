0b5ac04635b769ec80ecbeb165c85b1b988c673e
Brooks <brooks@pdrutah.com>
2022-09-09 12:34:58 -0600

    web: added accessors, models

---
+++ 13.web-framework/src/models/User.ts
@@ -0,0 +1,51 @@
+import { Eventing } from '../Eventing'
+import { Sync } from '../Sync'
+import { Attributes } from '../Attributes'
+import { AxiosResponse } from 'axios'
+
+export interface UserProps {
+  id?: number
+  name?: string
+  age?: number
+}
+
+const rootUrl = 'http://localhost:3000/users'
+
+export class User {
+  public events: Eventing = new Eventing()
+  public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl)
+  public attributes: Attributes<UserProps>
+
+  constructor(props: UserProps) {
+    this.attributes = new Attributes<UserProps>(props)
+  }
+
+  get on() {
+    return this.events.on
+  }
+
+  get trigger() {
+    return this.events.trigger
+  }
+
+  get get() {
+    return this.attributes.get
+  }
+
+  set(update: UserProps): void {
+    this.attributes.set(update)
+    this.events.trigger('change')
+  }
+
+  fetch(): void {
+    const id = this.attributes.get('id')
+
+    if (typeof id !== 'number') {
+      throw new Error('Cannot fetch without an id')
+    }
+
+    this.sync.fetch(id).then((res: AxiosResponse): void => {
+      this.set(res.data)
+    })
+  }
+}
